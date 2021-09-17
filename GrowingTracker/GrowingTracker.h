6237ef447e2825e615f8f5a471a618530a62dffa
YoloMao <a83506677@126.com>
2021-09-17 17:46:13 +0800

    fix: .h 中使用库引用的方式导入 GrowingAnalytics 文件

--- GrowingTracker/GrowingTracker.h
+++ GrowingTracker/GrowingTracker.h
@@ -17,7 +17,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-#import "GrowingDynamicProxy.h"
+#import <GrowingAnalytics/GrowingDynamicProxy.h>
 #import "GrowingTrackConfiguration+CdpTracker.h"
 
 NS_ASSUME_NONNULL_BEGIN
--- GrowingTrackerCore/CDP/GrowingResourceCustomEvent.h
+++ GrowingTrackerCore/CDP/GrowingResourceCustomEvent.h
@@ -17,7 +17,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-#import "GrowingCustomEvent.h"
+#import <GrowingAnalytics/GrowingCustomEvent.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
